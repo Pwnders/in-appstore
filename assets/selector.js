a61e57a7ea2382dc1a0a6f6d5f35bd666aeeb37b
CrafterPika <CrafterPika@users.noreply.github.com>
2020-06-16 07:56:29 +0200

    fix JS buttons

--- assets/selector.js
+++
@@ -1,7 +0,0 @@
-var urlMenu = document.getElementById('APKMenu');
-urlMenu.onchange = function(){
-	var userOption = this.options[this.selectedIndex];
-	if (userOption.value != "nothing"){
-		window.open(userOption.value, "APK Download", "");
-	}
-}
